2eaa85d189942467942cc795256356a85de84769
ydc_glb <goodMoring@qq.com>
2022-05-07 21:31:08 +0800

    yangdengchun first commit

---
+++ src/main/java/com/example/system/dao/gittest.java
@@ -0,0 +1,11 @@
+package com.example.system.dao;
+
+/**
+ * @author ydc
+ * @create 2022-05-07-21:22
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("hello git! my name is yang dengchun");
+    }
+}
