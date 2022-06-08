09874686eb1c66541007634cc8459cd73882ee94
Alanx25 <101056435+Alanx25@users.noreply.github.com>
2022-06-09 00:31:49 +0800

    api/index

---
+++ music-manage/src/api/index.js
@@ -0,0 +1,15 @@
+import {get,post} from "./http";
+
+// 判断管理员是否登录成功
+export const getLoginStatus = (params) => post(`admin/login/status`,params);
+
+
+//============歌手相关================
+//查询歌手
+export const getAllSinger =() => get(`singer/allSinger`);
+//添加歌手
+export const setSinger = (params) => post(`singer/add`,params);
+//编辑歌手
+export const updateSinger = (params) => post(`singer/update`,params);
+//删除歌手
+export const delSinger = (id) => get(`singer/delete?id=${id}`);
